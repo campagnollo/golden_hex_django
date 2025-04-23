123351bffd24c87d902222215d3b475209c05386
Eric <campagnollo2002@gmail.com>
2025-04-23 15:29:12 -0400

    added static file dirs

--- golden_hex_django_app/settings.py
+++ golden_hex_django_app/settings.py
@@ -110,8 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+STATIC_URL = '/golden_hex_django_app/myapp/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'golden_hex_django_app/myapp/static')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "golden_hex_django_app/myapp/static",
 # ]
